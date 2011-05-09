dabbca4f4f8f7d24f339ac6fbc1fcca53aec25a6
Mjumbe Wawatu Ukweli <mjumbewu@kwawatu.com>
2011-05-09 15:52:32 -0700

    Add notepad app on express.

---
+++ express-notepad/express_server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express.createServer();
+
+app.use(express.bodyParser());
+app.set('view options', {
+    layout: false
+})
+
+var notes = {};
+var count = 1;
+
+app.get('/notes', function(req, res) {
+    var note_id = req.query.id || '';
+    
+    if (note_id) {
+        note = notes[note_id];
+    } else {
+        note = {title: '', content: ''};
+    }
+    
+    res.render('notepad.ejs', {note: note, notes: notes, note_id: note_id});
+});
+
+app.post('/save_note', function(req, res) {
+    note_id = req.body.note_id || count++;
+    var note = {
+        title: req.body.note_title,
+        content: req.body.note_content
+    };
+    notes[note_id] = note;
+    
+    res.redirect('notes?id=' + note_id);
+});
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
