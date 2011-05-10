f8aa8a8de9d69bd668c77381d26a441954d561eb
John Mertens <john.mertens@gmail.com>
2011-05-10 12:54:27 -0700

    Add facebook login

--- express-notepad/express_server.js
+++ express-notepad/express_server.js
@@ -4,39 +4,119 @@
  * Requirments:
  *   npm install express
  *   npm install ejs
+ *   npm install cradle
  *
  * To run:
  *   node express_server.js
  */
 var cradle = require('cradle');
-var db = new(cradle.Connection)('mertonium.iriscouch.com').database('notez');
+var conn = new(cradle.Connection)('mertonium.iriscouch.com');
 var express = require('express');
-var app = express.createServer();
+var fbsdk = require('facebook-sdk');
+var connect = require('connect');
+var Step = require('step');
 
+var app = express.createServer();
+var db;
 var notes = {};
 var count = 1;
+var fbid;
+var facebook;
 
+// Express app setup
 app.use(express.bodyParser());
 app.set('view options', {
     layout: false
-})
-
-db.all({ 'include_docs':'true' }, function(err, data) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(data);
-        for(i=0; i < data.length; i++) {
-            notes[data[i].id] = data[i].doc;
-        }
-    }
-    console.log(notes);
 });
+
+// Load the db data
+var dataLoader = function() {
+    var db = conn.database('notez');
+    db.all({ 'include_docs':'true' }, function(err, data) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            for(i=0; i < data.length; i++) {
+                notes[data[i].id] = data[i].doc;
+            }
+        }
+        console.log(notes);
+    });
+};
+
+// Setup the hompage to be the login screen
+/*
+app
+*/
+
+app.get('/', function(req, res) {
+    // Begin Callback soup
+    console.log('in fbsetup');
+    // Do the facebook stuff
+    facebook = new fbsdk.Facebook({
+        appId: '144849692255128',
+        secret:'cb633c55662fb7666e4653be3c09387d',
+        request: req,
+        response:res
+    });
+    
+    
+    console.log('in checkDb');
+    // Are they logged in?
+    if(facebook.getSession()) {
+        console.log('already logged in');
+        facebook.api('/me', function(me) {
+            // Set the db (create if needed)
+            fbid = me.id;
+            console.log('fbid #1 = '+fbid);
+            db = conn.database('notez-'+fbid);
+            
+            console.log('look at me');
+            console.log('in setupDb');
+            db.create(function(err) {
+                console.log(arguments);
+                console.log('in loggedin');
+                console.log('made it');
+                
+                // Load the user's notes
+                db.all({ 'include_docs':'true' }, function(err, data) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log(data);
+                        for(i=0; i < data.length; i++) {
+                            notes[data[i].id] = data[i].doc;
+                        }
+                    }
+
+                    // Direct to the notes page
+                    res.redirect('notes?fbid='+fbid);
+                });
+            });
+        });
+    } else {
+        //  If the user is not logged in, just show them the login button.
+        res.end('<a href="' + facebook.getLoginUrl() + '">Login</a>');
+        console.log('click the link');
+    }
+});
+
+
+/*
+db.exists(function(req, res) { 
+    if(!res) { 
+        db.create(function(){}); 
+    }
+});
+*/
+
 //console.log(fakeNotes);
 
 app.get('/notes', function(req, res) {
     var note_id = req.query.id || '';
-    
+    var fbid = req.query.fbid || '';
+    console.log('fbid = '+fbid);
     if (note_id) {
         note = notes[note_id];
     } else {
